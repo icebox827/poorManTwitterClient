005dc3fa2c5b1669624cc3b8823971991bb38073
Denis Lafontant <icebox827@gmail.com>
2021-08-12 07:44:21 -0400

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class="navbar">
-        <h1>Poor Man's Twitter</h1>
-    </div>
+  <nav class="navbar">
+    <h1>Poor Man's Twitter</h1>
+  </nav>
 </template>
+
+<style>
+  .navbar{
+    background-color: rgb(61, 61, 250);
+    color: white;
+    height: 5vh;
+  }
+
+  .navbar h1 {
+    text-align: center;
+    background-color: rgb(61, 61, 250);
+  }
+</style>
