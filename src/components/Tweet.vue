fb381271c47a9190c72ed3624443605605bcd398
Denis Lafontant <icebox827@gmail.com>
2021-08-12 08:23:53 -0400

    Create form

---
+++ src/components/Tweet.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <form action="" id="tweetForm" class="form">
+      <input type="text" name="name" id="name" placeholder="Your name" class="name">
+      <input type="text" name="inputTweet" id="inputTweet" placeholder="Tweet here" class="tweet">
+      <input type="button" id="tweet" value="Tweet" class="btn">
+    </form>
+  </div>
+</template>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    margin-top: 60px;
+  }
+
+  .form input {
+    margin: 10px;
+    border-radius: 5px;
+  }
+
+  .form .tweet {
+    width: 60vh;
+  }
+
+  .form .btn {
+    background-color: rgb(61, 61, 250);
+    color: white;
+  }
+</style>
